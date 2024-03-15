cd92a756be43964eb42e5c2bb0ed1a1fad1f3e73
hannechi <hannechichiheb@gmail.com>
2024-03-15 23:23:49 +0100

    add front

---
+++ frontAppUspn/ckeditor5-41.0.0-hx9ric8903g3/sample/script.js
@@ -0,0 +1,20 @@
+ClassicEditor
+	.create( document.querySelector( '.editor' ), {
+		// Editor configuration.
+	} )
+	.then( editor => {
+		window.editor = editor;
+	} )
+	.catch( handleSampleError );
+
+function handleSampleError( error ) {
+	const issueUrl = 'https://github.com/ckeditor/ckeditor5/issues';
+
+	const message = [
+		'Oops, something went wrong!',
+		`Please, report the following error on ${ issueUrl } with the build id "hx9ric8903g3-2txty0oq2j5j" and the error stack trace:`
+	].join( '\n' );
+
+	console.error( message );
+	console.error( error );
+}
