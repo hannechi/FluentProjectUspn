88d505f4aaae07d4b8bf7fdc8a6dbd3c0c2b979f
hannechi <hannechichiheb@gmail.com>
2024-04-07 21:23:49 +0200

    backendUser

--- frontAppUspn/src/app/authentification/sign-in/sign-in.component.ts
+++ frontAppUspn/src/app/authentification/sign-in/sign-in.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit,Renderer2, ElementRef } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
+import { User } from 'src/app/models/User';
 import { Userauth } from 'src/app/models/Userauth';
 import { UserService } from 'src/app/service/user.service';
 import Swal from 'sweetalert2';
@@ -12,14 +13,13 @@ import Swal from 'sweetalert2';
 export class SignInComponent implements OnInit{
 
   incorrectInfo=false;
- 
+  passwordError = false;
+  emailerror =false;
+  usernameError = false;
   constructor(private activeModal: NgbActiveModal,private renderer: Renderer2, private elementRef: ElementRef,private userservice : UserService) { 
 
   }
-  validateEmail(email: string): boolean {
-    const emailRegex: RegExp = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-    return emailRegex.test(email);
-}
+
   ngOnInit(): void {
  
 
@@ -34,20 +34,66 @@ export class SignInComponent implements OnInit{
   }
   SignUpForm(SignUpForm:NgForm)
   {
-    
+   
     let username = SignUpForm.form.value.Username;
     let password = SignUpForm.form.value.Password;
-    let email = SignUpForm.form.value.email;
+    let email = SignUpForm.form.value.Email;
     let Passwordagain = SignUpForm.form.value.Passwordagain;
-    if (username != "" && password!="" && email != "" && Passwordagain !="") {
-      if (password==Passwordagain)
+    if (username && password && email && Passwordagain) {
+      if (password!=Passwordagain)
       {
-        alert("hello wolrd")
-       if (this.validateEmail(email))
-       {
-        alert("hello wolrd 2")
-       }
+          this.passwordError=true;
       }
+      else
+      {
+        this.passwordError=false;
+      }
+      let user = new User(email,username,password,"student")
+      console.dir(user)
+      this.userservice.signup(user).subscribe(
+        {
+          next:(data)=>
+            {
+              this.emailerror=false;
+              this.passwordError=false;
+              this.usernameError = false;
+              Swal.fire({
+                position: "top-end",
+                icon: "success",
+                title: "you are successfully registered",
+                showConfirmButton: false,
+                timer: 1500
+              });
+
+            },
+          error:(err)=>
+            {
+              if (err.error =="Un compte avec cet e-mail existe déjà.")
+              {
+
+                this.emailerror=true;
+                this.usernameError = false;
+
+              }
+              else if (err.error == "Ce nom d'utilisateur est déjà utilisé.")
+                {
+                  this.usernameError = true;
+                  this.emailerror=false;
+                }
+                else
+                {
+                  Swal.fire({
+                    position: "top-end",
+                    icon: "error",
+                    title: "Something went wrong! try later",
+                    showConfirmButton: false,
+                    timer: 1500
+                  });
+                }
+
+            }
+        }
+      )
     }
 
    
